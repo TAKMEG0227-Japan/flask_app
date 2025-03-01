8d3ddea850b01fc85d766333220207b4ea166372
TAKMEG0227-japan <sleeping.lion.0227@gmail.com>
2025-03-01 19:06:23 +0900

    Fix deployment

--- src/View.py
+++ src/View.py
@@ -1,14 +1,19 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# coding: utf-8
 
 from flask import Flask, render_template
 
+# app という変数でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
 
+# --- View 側の設定 ---
+# rootディレクトリにアクセスした場合の挙動
 @app.route('/')
-def index():
-    return render_template('index.html')
 
+# def 以下がアクセス後の操作
+def index():
+    # return 'Hello World!'
+    return render_template('index.html') #追加
+
+# メイン関数
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
